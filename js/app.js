5ca3839105447bec937cc9480fddb96a3d40ae04
sumonbarai <sumonbarai78@gmail.com>
2024-05-25 13:49:16 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ const otpValidation = document.getElementById("otp-validation");
 
 let dynamicCode;
 let ourOtp = "";
+let optTime = 15;
+let timerId;
+
 // function
 function changeTheme() {
   const isLight = body.classList.contains("light-bg");
@@ -24,7 +27,7 @@ function changeTheme() {
   }
 }
 
-function checkOTP(e) {
+function inputOTP(e) {
   const currentElement = e.target;
   const currentValue = e.target.value;
   const nextChild = currentElement.nextElementSibling;
@@ -39,25 +42,11 @@ function checkOTP(e) {
   if (nextChild) {
     nextChild.focus();
   }
-
-  // checking otp is valid or not
   ourOtp = ourOtp + currentValue;
-  const result = Number(ourOtp) === dynamicCode;
-  if (result) {
-    otpSuccess();
-  } else {
-    otpFail();
-  }
 }
 
 function generateOTP() {
-  const generatedOTP = document.getElementById("generated-otp");
-  dynamicCode = Math.floor(1000 + Math.random() * 9000);
-  generatedOTP.innerHTML = ` 
-  <span>OTP code is : ${dynamicCode}</span>
-  <button class="btn" onclick="generateOTP()">reset</button>
-  `;
-  otpValidation.innerHTML = "";
+  return Math.floor(1000 + Math.random() * 9000);
 }
 
 function otpSuccess() {
@@ -65,14 +54,55 @@ function otpSuccess() {
   otpValidation.classList.add("success");
   otpValidation.innerText = "OTP has been validated successfully";
 }
+
 function otpFail() {
   otpValidation.classList.remove("success");
   otpValidation.classList.add("fail");
   otpValidation.innerText = "OTP does not match";
 }
 
+function validateOtp() {
+  return dynamicCode === Number(ourOtp);
+}
+
+function checkOTP(e) {
+  inputOTP(e);
+  const result = validateOtp();
+  if (result) {
+    otpSuccess();
+  } else {
+    otpFail();
+  }
+}
+
+function displayOTP() {
+  const generatedOtp = document.getElementById("generated-otp");
+  dynamicCode = generateOTP();
+
+  timerId = setInterval(() => {
+    if (optTime < 1) {
+      dynamicCode = null;
+      return clearInterval(timerId);
+    }
+    optTime = optTime - 1;
+    generatedOtp.innerHTML = `<div>Our otp code is ${dynamicCode}</div>
+    <button class="btn" onclick="reset()">reset</button>
+    <div>expires in ${optTime}s</div>`;
+  }, 1000);
+}
+
+function reset() {
+  const generatedOtp = document.getElementById("generated-otp");
+  clearInterval(timerId);
+  dynamicCode = null;
+  ourOtp = "";
+  optTime = 15;
+  generatedOtp.innerHTML = "Loading...";
+  setTimeout(displayOTP, 1000);
+}
+
 // EventListener
 switchBtn.addEventListener("click", changeTheme);
 otpInputBoxList.addEventListener("input", checkOTP);
 
-setTimeout(generateOTP, 1000);
+setTimeout(displayOTP, 1000);
